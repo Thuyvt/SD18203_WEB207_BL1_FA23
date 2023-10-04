9747d645933658ef328119ae6fac122f72e82204
thuyvt <thuyvt1005@gmail.com>
2023-10-04 17:52:28 +0700

    ab

--- GD3_CRUD/Day10/index.js
+++ GD3_CRUD/Day10/index.js
@@ -17,16 +17,48 @@ app.controller("listPostsController", function ($scope, $http, $routeParams){
 
     // NẾU TRÊN ĐƯỜNG DẪN CÓ PARAMETER
     $scope.id = $routeParams.id;
+    $scope.detail = {};
+    $scope.dsThuyvt66 = [];
     if ($scope.id && $scope.id > 0) {
         console.log($scope.id);
          // Gửi yêu cầu lấy ra danh sách
         $http.get(api_post+"/" + $scope.id) // gửi yêu cầu với method get đến api để lấy ra dữ liệu
         .then(function (response) { // sau khi có dữ liệu, xử lý dữ liệu trả về
-            console.log(response.data);
+            $scope.detail = response.data;
         })
         .catch(function (error) {
             alert(error.message);
         })
+    } else if ($scope.id === "thuyvt66") {
+        $http.get(api_post + "?author=thuyvt66")
+        .then(function (response) {
+            $scope.dsThuyvt66 = response.data;
+        })
+        .catch(function (error) { });
+    }
+
+    // xóa đối tượng
+    $scope.delete = function (index) {
+        let id = $scope.lstPosts[index].id;
+        $http.delete(api_post + "/" + id)
+        .then(function (response) {
+            // xóa luôn dòng dữ liệu trên bảng
+            $scope.lstPosts.splice(index, 1);
+            // alert("Xóa thành công");
+        })
+        .catch(function (error) {});
+    }
+
+    // Chọn đối tượng cần sửa
+    $scope.post = {};
+    $scope.onSelect = function (index) {
+        let id = $scope.lstPosts[index].id;
+        $http.get(api_post + "/"+ id)
+        .then(function (response) {
+            $scope.post = response.data;
+            console.log($scope.post);
+        })
+        .catch(function (error) {});
     }
 });
 
